1657eecf284c9389b40f0a8011332511d81d1f53
luizfpinheiro <luiz.pinheiro@cultura.gov.br>
2018-09-15 17:50:01 -0300

    Module I (Naive Bayes) - Classificação, Treinamento, Previsão e Precisão
    - Criando um classificador e treinando
    - Prevendo valores com o classificador treinado
    - Medindo precisão do valor previsto pelo classificador treinado

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+clf = GaussianNB()
 
+t_clf = time()
+clf.fit(features_train, labels_train) ## train clf
+print 'tempo de treinamento clf: ', round(time()-t_clf, 3), "s"
+
+t_pred = time()
+pred = clf.predict(features_test) ## predict values
+print 'tempo de previsao: ', round(time()-t_pred, 3), "s"
+
+# score = clf.score(features_test, labels_test)
+accuracy = accuracy_score(pred, labels_test) ## test accuracy 
+print 'Precisao do classificador: ', accuracy, '%'
 #########################################################
 
 
